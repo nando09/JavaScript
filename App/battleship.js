f58ba791b2aaab84482ae5857d5ab4a02aab5d5c
nando09 <nando.online.fb@gmail.com>
2017-11-23 00:12:03 -0200

    pegando os palpites do jogador

--- App/battleship.js
+++ App/battleship.js
@@ -24,7 +24,7 @@ var model = {
 			{location: ["10", "11", "12"], hits: ["", "", ""]}],
 
 	fire: function(guess){
-		for (var i = 0; i < this.numShips; i++) {
+		for (var i = 0; i < this.numShips; i++){
 			var ship = this.ships[i];
 			var index = ship.location.indexOf(guess);
 			if(index >= 0){
@@ -54,14 +54,22 @@ var model = {
 }
 
 var controller = {
+	//Palpites
 	guesses: 0,
 
+	//function para pegar informação do tiro
 	processGuess: function(guess){
+		//verificando na funcao se o guess esta conforme o tabuleiro
 		var location = parseGuess(guess);
+		//Se o retorno de location for verdadeiro entra no if
 		if(location){
+			//acrecente mais 1 no guesses
 			this.guesses++;
+			//retorna true se o navio for atingido
 			var hit = model.fire(location);
+			//Se o navio for atingido e foi todos eles entra no if
 			if(hit && model.shipsSunk === model.numShips){
+				//Manda uma mensagem para o usuario
 				view.displayMessage("Você derrubou todos os navios, in "+ this.guess +" guesses");
 			}
 		}
@@ -71,20 +79,28 @@ var controller = {
 function parseGuess(guess){
 	var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
 	if(guess === null || guess.length !== 2){
-		alert("Oops, please enter a letter and a number on the board.");
+		alert("Oops, Por favor, entre com uma letra e um numero.");
 	}else{
+		//Pega o primeiro caracter do guess
 		var firsChar = guess.charAt(0);
+		//Tras do firsChar o primeiro caracter, e procura no alphabet a letra correspondente
+		//Quando achara letra no firsChar, retorna o numero do indice
 		var row = alphabet.indexOf(firsChar);
+		//Pega o segundo caracter do guess
 		var column = guess.charAt(1);
-
+		//row e column não é numero?
 		if(isNaN(row) || isNaN(column)){
 			alert("Oops, that isn't on the board.");
+			//row é meno que zero ou maior que model.boardSize "(7)"
+			//column e meno que zero ou maior que model.boardSize "(7)"
 		}else if(row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize){
-			alert("Oops, that's off the board!");
+			alert("Oops, Não existe essa opção!");
 		}else{
-			return row+column;
+			//Retorna as entradas de valores convertendo para numeros
+			return row + column;
 		}
 	}
+	//Caso tenho algum erro localizado ele retorna null
 	return null;
 }
 
@@ -102,6 +118,7 @@ controller.processGuess("E4");
 controller.processGuess("B0");
 controller.processGuess("B1");
 controller.processGuess("B2");
+
 
 /*var ships1 = [{location: ["10", "20", "30"], hits: ["", "", ""]},
 			  {location: ["32", "33", "34"], hits: ["", "", ""]},
